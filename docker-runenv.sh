b40c68841e791c38748aed3f01ce05ef113be989
ling.shi <ling.shi@horizon.ai>
2019-11-04 23:09:30 +0800

    optimize

--- docker-runenv.sh
+++ docker-runenv.sh
@@ -1,12 +1,13 @@
 docker ps -a -f name=$ContainerName | grep -c $ContainerName
 if [ $? = 1 ]; then
     xhost +local:root && \
-    nvidia-docker run --rm -it \
+    nvidia-docker run $OtherParam \
+    --rm -it \
     --name $ContainerName \
     -h $ContainerName \
     -e DISPLAY \
     -e QT_X11_MITSHM=1 \
-    -v /tmp/.X11-unix/X1:/tmp/.X11-unix/X1 \
+    -v /tmp/.X11-unix/X0:/tmp/.X11-unix/X0 \
     -v $HOME/code/HorizonAD:/root \
     -w /root \
     runenv:latest \

01f921e3edb07a4eb7be2fca77f1d265ea3f3258
ling.shi <ling.shi@horizon.ai>
2019-11-08 14:27:35 +0800

    optimize envparam DISPLAY, don't need to edit X num if DISPLAY is not :0.

--- docker-runenv.sh
+++ docker-runenv.sh
@@ -1,15 +1,15 @@
-docker ps -a -f name=$ContainerName | grep -c $ContainerName
-if [ $? = 1 ]; then
+if [ $(docker ps -a -f name=$ContainerName | grep -c $ContainerName) = 0 ]; then
+    export DisplayNum=$(echo $DISPLAY | grep '[^:]' -o) && \
     xhost +local:root && \
-    nvidia-docker run $OtherParam \
-    --rm -it \
+    nvidia-docker run --rm -it \
     --name $ContainerName \
     -h $ContainerName \
     -e DISPLAY \
     -e QT_X11_MITSHM=1 \
-    -v /tmp/.X11-unix/X0:/tmp/.X11-unix/X0 \
+    -v /tmp/.X11-unix/X$DisplayNum:/tmp/.X11-unix/X$DisplayNum:rw \
     -v $HOME/code/HorizonAD:/root \
     -w /root \
+    $ExtrarParam \
     runenv:latest \
     /bin/bash
 else
